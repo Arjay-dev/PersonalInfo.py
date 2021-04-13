96b38b2390bf3ccfe79899541b7455c38fd024bc
Arjay-dev <82429247+Arjay-dev@users.noreply.github.com>
2021-04-13 11:53:13 +0800

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,14 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> firstname = input("Enter first name : ")
+Enter first name : Arjay
+>>> lastname = input("Enter last name : ")
+Enter last name : Unabia
+>>> location = input("Enter location : ")
+Enter location : Cavite
+>>> age = input("Enter age : ")
+Enter age : 20
+>>> space = " "
+>>> print(firstname+space+lastname+space+location+space+age)
+Arjay Unabia Cavite 20
+>>> 
